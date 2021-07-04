617863297e3b1ee619df57e2271d9e1938481345
piyushSharma2 <76560083+piyushSharma2@users.noreply.github.com>
2021-07-04 16:38:16 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,16 @@
+class Box{
+    constructor(x,y,width,height){
+        var Options={
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,width,height,Options)
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body)
+    }
+    display(){
+        
+        rectMode(CENTER)
+        rect(this.body.position.x,this.body.position.y,this.width,this.height)
+    }
+}
